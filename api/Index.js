576248a28db54de26a0060c8f47d380c7c0f3187
Ruijin Wang <cpmunichwrj@gmail.com>
2021-12-09 17:35:52 -0600

    regex

--- api/Index.js
+++ api/Index.js
@@ -18,7 +18,7 @@ app.get("/api/hello", (req, res) => {
 });
 
 app.use(express.static(path.join(__dirname, '../frontend/build')))
-app.get(/^\/(?!api).*/, (req, res) => {
+app.get(/^\/(?!(api|auth)).*/, (req, res) => {
     res.sendFile(path.join(__dirname + '/../frontend/build/index.html'))
 })
 
